587f05f8c0bafe8507f51060a261febf6318f7e9
Resi Respati <resir014@gmail.com>
2022-12-05 16:07:14 +0700

    refactor: remove unused variable

--- src/modules/flightsim-telemetry/utils/telemetry-replicant.ts
+++ src/modules/flightsim-telemetry/utils/telemetry-replicant.ts
@@ -1,7 +1,5 @@
 import { FlyLiveFlightData } from '../types';
 
-export const TELEMETRY_REPLICANT_NAMESPACE = 'resir014-flylive';
-
 const defaultValue: FlyLiveFlightData = {};
 
 export const telemetryReplicantOpts = {
